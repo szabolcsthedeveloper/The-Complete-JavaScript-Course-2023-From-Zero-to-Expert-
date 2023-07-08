4298d6fb96ae97012af43b9f83d7ec7c754a7f34
Szabolcs Nagy <nagyszabolcs4455@gmail.com>
2023-07-08 23:12:32 +0200

    fundamentals 75% completed

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,50 @@
+/*
+// Values and Variables
+
+let country = 'Hungary';
+let continent = 'Europe';
+let population = 10;
+
+console.log(country, continent, population)
+ 
+
+// Data Types
+
+let isIsland = false;
+const language = 'Hungarian'
+
+console.log(isIsland, population, continent, language)
+
+
+// Let, const and var
+// changes to above
+
+
+// Basic Operators
+
+console.log(population / 2);
+
+population ++;
+console.log(population);
+
+const Finland = 6;
+console.log(population > Finland);
+
+const avgPop = 33;
+console.log(population > avgPop);
+
+const description = 'Hungary is in Europe, and its 10 million people speak Hungarian'
+
+
+
+const descriptionNew = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+
+console.log(descriptionNew)
+
+if (population >= 33) {
+    console.log(`${country}'s population is above average`)
+} else {
+    console.log(`${country}'s population of ${population} million is below average`)
+}
+
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,231 @@
+//Variable name conventions
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Szabolcs';
+console.log(firstName);
+
+let szabolcsNagy = 'SZN';
+let $function = 27;
+
+let person = 'szabolcs';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+
+///////////////////////////////////////////
+//Data Types in JavaScript
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Szabolcs');
+
+javaScriptIsFun = 'YES';
+console.log(javaScriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+///////////////////////////////////////////
+// Const, Let and Var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Nagy'
+console.log(lastName)
+
+
+///////////////////////////////////////////
+// Basic Operators
+
+const currentYear = 2023;
+const ageSzabolcs = currentYear - 2003;
+const ageFanni = currentYear - 2004;
+console.log(ageSzabolcs, ageFanni)
+
+console.log(ageSzabolcs * 2, ageSzabolcs / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Szabolcs';
+const lastName = 'Nagy';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4; 
+x ++;
+x --;
+console.log(x);
+
+console.log(ageSzabolcs > ageFanni);
+console.log(0ageSzabolcs >= 18);
+
+const currentYear = 2023;
+const ageSzabolcs = currentYear - 2003;
+const ageFanni = currentYear - 2004;
+console.log(ageSzabolcs, ageFanni)
+
+console.log(currentYear - 1991 > currentYear - 2023);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageFanni + ageSzabolcs) / 2
+console.log(ageFanni, ageSzabolcs, averageAge)
+
+
+
+
+
+// #1 Coding Challange 
+
+let weightMark = 78;
+let weightJohn = 92;
+
+let heightMark = 1.69;
+let heightJohn = 1.95;
+
+let markBMI = weightMark / (heightMark * heightMark);
+let johnBMI = weightJohn / heightMark ** 2;
+console.log( markBMI, johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI)
+
+weightMark = 95;
+weightJohn = 85;
+
+heightMark = 1.88;
+heightJohn = 1.76;
+
+markBMI = weightMark / (heightMark * heightMark);
+johnBMI = weightJohn / heightMark ** 2;
+console.log(markBMI, johnBMI);
+
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI)
+
+
+const firstName = 'Szabolcs';
+const job = 'programmer';
+const birthYear = 2003;
+const year = 2023
+
+const szabolcs = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+
+console.log(szabolcs);
+
+const szabolcsNew = `I'm ${firstName}, a ${(year - birthYear)} years old ${job}!`;
+console.log(szabolcsNew);
+
+console.log(`Just a regular string..`)
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`)
+
+
+const age = 14
+
+if (age >= 17) {
+    console.log('Sarah can start her driving license');
+} else {
+    const yearsLeft = 17 - age;
+    console.log(`Sarah needs to wait ${yearsLeft} years to start...`);
+}
+
+const birthYear = 2001;
+
+let century;
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's`)
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's`)
+}
+
+
+const inputYear = '1991';
+console.log(Number(inputYear) + 18);
+console.log(inputYear + 18);
+
+console.log(Number('Szabolcs'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+
+ let n = '1' + 1;
+ n = n - 1;
+ console.log(n);
+
+
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 -2);
+
+
+*/
+
+
+
+
+
+
+
+
